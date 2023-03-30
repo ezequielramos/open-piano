c70eec9a02934abc798c1502d3b2281d8e9fb7ff
Ezequiel Ramos <ezequielmr94@gmail.com>
2023-03-30 02:58:18 -0300

    ok

--- App.tsx
+++ App.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import Canvas from './src/Canvas';
 
 export default function App() {
